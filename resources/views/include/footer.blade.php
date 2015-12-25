4accd18558f7c8a34e470add9a3bbe8e4bd1c945
Peijin Sun <Stefanie@Peijins-MBP.attlocal.net>
2015-12-25 01:48:13 -0800

    ..

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -2,6 +2,12 @@
 		<script src="js/jquery.js"></script>
 		<!-- Bootstrap Core JavaScript -->
 		<script type="text/javascript">
+		$("a.cheap-title").each(function() {
+			if ($(this).text().length > 8) {
+				$(this).text($(this).text().substring(0, 8));
+				$(this).html($(this).html() + '...');
+			}
+		});
 		$("a.post-content").each(function() {
 			var maxwidth = 100;
 			if ($(this).text().length > maxwidth) {
@@ -9,12 +15,7 @@
 				$(this).html($(this).html() + '...');
 			};
 		});
-		$("a.cheap-title").each(function() {
-			if ($(this).text().length > 8) {
-				$(this).text($(this).text().substring(0, 8));
-				$(this).html($(this).html() + '...');
-			}
-		});
+		
 		</script>
 		<script src="js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="js/jQuery.paginate.js"></script>

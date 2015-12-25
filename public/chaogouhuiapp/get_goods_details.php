1596cf227e4bb04b438113af96c8032e6c2113e6
peijinsun <sunpeijin7@gmail.com>
2015-12-25 17:45:30 +0800

    change cheaps title limit

--- public/chaogouhuiapp/get_goods_details.php
+++ public/chaogouhuiapp/get_goods_details.php
@@ -3,16 +3,16 @@ require("db_config.php");
 
 $choiceId = $_POST['choiceid'];
 //$choiceId = '20151003018';
-$sql="UPDATE goods_info SET skim_counts = skim_counts + 1 WHERE code =".$choiceId.";";
+$sql="UPDATE goods_info SET skim_counts = skim_counts + 1 WHERE code ='".$choiceId."';";
 $result = mysql_query($sql, $con);
 
-$sql="select description from goods_info where goods_info.code = ".$choiceId.";";
+$sql="select description from goods_info where goods_info.code = '".$choiceId."';";
 $result = mysql_query($sql, $con);
 $resultArr = array();
 $row = mysql_fetch_array($result, MYSQL_ASSOC); // return detail information of the product including details
 $resultArr = $row;
 
-$sql = "select image_name from goods_images where goods_code = ".$choiceId.";";
+$sql = "select image_name from goods_images where goods_code = '".$choiceId."';";
 $result=mysql_query($sql,$con);
 $images = array();
 $index = 0;
